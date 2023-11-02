da531209bf9e3303bd780a6505bedabb0fe4b639
MaryelN <maryel.navarromtz@gmail.com>
2023-11-02 18:59:43 +0100

    add easyadmin-bundle & gallery Route

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GalleryController extends AbstractController
 {
-    #[Route('/gallery', name: 'app_gallery')]
+    #[Route('/gallerie', name: 'app_gallery')]
     public function gallery(): Response
     {
-        return $this->render('gallery/index.html.twig', [
-            'controller_name' => 'PagesController',
-            'title' => 'Gallery',
+        return $this->render('gallery/index.html.twig');
+    }
+    #[Route('/gallerie/{id}', name: 'app_gallery_details')]
+    public function details(Car $car): Response
+    {
+        dd($car);
+        Return $this->render('gallery/details.html.twig', [
+            'controller_name' => 'Gallerie Details Controller',
+            'title' => 'Details',
         ]);
     }
 }
