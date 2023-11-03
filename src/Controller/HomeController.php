24db6f2f62a846dcf5c941ec25fb27368cee0659
MaryelN <maryel.navarromtz@gmail.com>
2023-11-03 23:17:02 +0100

    Insert of data from serviceRepository to home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/accueil', name: 'app_home')]
-    public function index(): Response
+    public function index(ServiceRepository $serviceRepository): Response
     {
-        //manipuler des données + logique
-        
+        $services = $serviceRepository->findBy([], ['id' => 'ASC']);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'services'=>$services
         ]);
     }
+    
 }
