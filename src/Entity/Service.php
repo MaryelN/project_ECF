279c6ede539be263a45c17469144346a902601d6
MaryelN <maryel.navarromtz@gmail.com>
2023-11-09 00:49:14 +0100

    fix of service controller & add of dataFixtures

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -2,12 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\ServicesRepository;
+use App\Repository\ServiceRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ServicesRepository::class)]
-class Services
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
