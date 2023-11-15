2ea90578853da7d02d7548cbbcdb847f1fc7b8e7
MaryelN <maryel.navarromtz@gmail.com>
2023-11-15 13:01:34 +0100

    add form & send function to contact in gallery

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Car;
+use App\Entity\Contact;
+use App\Form\ContactCarType;
 use App\Repository\CarRepository;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,4 +37,43 @@ class GalleryController extends AbstractController
         ]);
     }
 
+    #[Route('/{id}/contacter', name:'contact')]
+    public function sendMessage(Request $request, EntityManagerInterface $manager, SendMailService $mailer, Car $car): Response
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactCarType::class, $contact, [
+            'subject' => $car->getName()
+        ]);
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+
+            $carName = $car->getName();
+            // $contact->setSubject($carName);
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $mailer->send(
+            $contact->getEmail(),
+            'admin@garage.com',
+            $contact->getSubject(),
+            $template = 'contactCar',
+            compact('contact', 'car')
+            );
+
+            $this->addFlash('success', 'Votre message a bien été envoyé !');
+
+            return $this->redirectToRoute('app_gallery_index');
+        }
+        
+        return $this->render('gallery/contact.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Contactez-nous',
+            'car' => $car
+        ]);
+    }
 }
