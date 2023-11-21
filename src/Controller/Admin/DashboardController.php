e957bbaadaadebf1ed45db5f8deb7a14111852c8
MaryelN <maryel.navarromtz@gmail.com>
2023-11-21 16:01:27 +0100

    add of meta and Roles in Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,12 +13,16 @@ use App\Entity\Comment;;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas accès à cette page')]
+    #[IsGranted('ROLE_USER', message: 'Vous n\'avez pas accès à cette page')]
+
     public function index(): Response
     {
         if (!$this->getUser()) {
@@ -35,23 +39,29 @@ class DashboardController extends AbstractDashboardController
             ->renderContentMaximized();
     }
 
+    
     public function configureMenuItems(): iterable
     {
-        return [
+        $menuItems = [
             MenuItem::linkToDashboard('Accueil', 'fa fa-home'),
-
-            MenuItem::section('Utilisateurs'),
-            MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class),
-
-            MenuItem::section('Site Garage'),
-            MenuItem::linkToCrud('Horaires', 'fa fa-clock', Schedule::class),
-            MenuItem::linkToCrud('Messages', 'fa fa-envelope', Contact::class),
-            MenuItem::linkToCrud('Commentaires', 'fa fa-comment', Comment::class),
-            MenuItem::linkToCrud('Services', 'fa fa-wrench', Service::class),
-            MenuItem::section('Voitures d\'Occasion'),
-            MenuItem::linkToCrud('Annonces de Voitures', 'fa fa-car', Car::class),
-            MenuItem::linkToCrud('Marques de Voitures', 'fa fa-check', Brand::class),
-            MenuItem::linkToCrud('Images des Voitures', 'fa fa-photo', Thumbnail::class),
         ];
+    
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $menuItems[] = MenuItem::section('Utilisateurs');
+            $menuItems[] = MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
+        }
+    
+        $menuItems[] = MenuItem::section('Site Garage');
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $menuItems[] = MenuItem::linkToCrud('Horaires', 'fa fa-clock', Schedule::class);
+        }
+        $menuItems[] = MenuItem::linkToCrud('Messages', 'fa fa-envelope', Contact::class);
+        $menuItems[] = MenuItem::linkToCrud('Commentaires', 'fa fa-comment', Comment::class);
+        $menuItems[] = MenuItem::linkToCrud('Services', 'fa fa-wrench', Service::class);
+    
+        $menuItems[] = MenuItem::section('Voitures d\'Occasion');
+        $menuItems[] = MenuItem::linkToCrud('Annonces de Voitures', 'fa fa-car', Car::class);
+    
+        return $menuItems;
     }
 }
