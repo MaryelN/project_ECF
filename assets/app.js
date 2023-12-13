a856f56a5a5a615f9d9c9db28683a540c7bcf700
MaryelN <maryel.navarromtz@gmail.com>
2023-12-13 18:45:31 +0100

    add of sass folder ans setup for style

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 function generateStarRating(rating) {
   let stars = '';
